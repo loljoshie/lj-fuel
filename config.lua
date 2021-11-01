9c4519fa3bfb13f814cd2a2f2a64fb2029fdc729
loljoshie <91661118+loljoshie@users.noreply.github.com>
2021-10-31 23:16:40 -0600

    v1.1

--- config.lua
+++ config.lua
@@ -1,9 +1,14 @@
 Config = {}
 
+Config.ShowNearestGasStationOnly = true -- show nearest gas stations when close enough
+Config.ShowAllGasStations = false -- show all gas stations around map
+Config.LeaveEngineRunning = true -- when set to true vehicle engine will run upon exiting vehicle
+Config.VehicleBlowUp = true -- when set to true vehicle has a chance to blow up if engine is left running
+Config.BlowUpChance = 40 -- percentage for chance of engine explosion
 Config.RefillCost = 100 -- default price
 Config.CostMultiplier = 3.0 -- tax basically
-Config.canCost = 400 --buy jerry can price
-Config.refuelCost = 600 --refueling jerry can price
+Config.canCost = 400 -- buy jerry can price
+Config.refuelCost = 600 -- refueling jerry can price
 
 Config.FuelDecor = "_FUEL_LEVEL" -- don't touch
 
@@ -54,6 +59,37 @@ Config.FuelUsage = {
 	[0.2] = 0.1,
 	[0.1] = 0.1,
 	[0.0] = 0.0,
+}
+
+Config.GasStationsBlips = { -- gas station blips
+    vector3(49.4187, 2778.793, 58.043),
+    vector3(263.894, 2606.463, 44.983),
+    vector3(1039.958, 2671.134, 39.550),
+    vector3(1207.260, 2660.175, 37.899),
+    vector3(2539.685, 2594.192, 37.944),
+    vector3(2679.858, 3263.946, 55.240),
+    vector3(2005.055, 3773.887, 32.403),
+    vector3(1687.156, 4929.392, 42.078),
+    vector3(1701.314, 6416.028, 32.763),
+    vector3(179.857, 6602.839, 31.868),
+    vector3(-94.4619, 6419.594, 31.489),
+    vector3(-2554.996, 2334.40, 33.078),
+    vector3(-1800.375, 803.661, 138.651),
+    vector3(-1437.622, -276.747, 46.207),
+    vector3(-2096.243, -320.286, 13.168),
+    vector3(-724.619, -935.1631, 19.213),
+    vector3(-526.019, -1211.003, 18.184),
+    vector3(-70.2148, -1761.792, 29.534),
+    vector3(265.648, -1261.309, 29.292),
+    vector3(819.653, -1028.846, 26.403),
+    vector3(1208.951, -1402.567,35.224),
+    vector3(1181.381, -330.847, 69.316),
+    vector3(620.843, 269.100, 103.089),
+    vector3(2581.321, 362.039, 108.468),
+    vector3(176.631, -1562.025, 29.263),
+    vector3(176.631, -1562.025, 29.263),
+    vector3(-319.292, -1471.715, 30.549),
+    vector3(1784.324, 3330.55, 41.253)
 }
 
 Config.GasStations = { -- gas station polyzones
