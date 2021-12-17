bc6bfc48d5ae4881a39fb2d1a90a8984b447f1f8
vanillaa <80897991+evanillaa@users.noreply.github.com>
2021-12-17 04:04:21 +0200

    optimize & refactor & change table &add new export

--- functions/functions_client.lua
+++ functions/functions_client.lua
@@ -19,40 +19,21 @@ function LoadAnimDict(dict)
 	end
 end
 
-function DrawText3Ds(x, y, z, text)
-	local onScreen,_x,_y=World3dToScreen2d(x,y,z)
-
-	if onScreen then
-		SetTextScale(0.35, 0.35)
-		SetTextFont(4)
-		SetTextProportional(1)
-		SetTextColour(255, 255, 255, 215)
-		SetTextEntry("STRING")
-		SetTextCentre(1)
-		AddTextComponentString(text)
-		DrawText(_x,_y)
-	end
-end
-
 function Round(num, numDecimalPlaces)
 	local mult = 10^(numDecimalPlaces or 0)
-
 	return math.floor(num * mult + 0.5) / mult
 end
 
 function CreateBlip(coords)
 	local blip = AddBlipForCoord(coords)
-
 	SetBlipSprite(blip, 361)
-	SetBlipScale(blip, 0.9)
+	SetBlipScale(blip, 0.6)
 	SetBlipColour(blip, 4)
 	SetBlipDisplay(blip, 4)
 	SetBlipAsShortRange(blip, true)
-
 	BeginTextCommandSetBlipName("STRING")
 	AddTextComponentString("Gas Station")
 	EndTextCommandSetBlipName(blip)
-
 	return blip
 end
 
@@ -64,9 +45,8 @@ function FindNearestFuelPump()
 
 	repeat
 		if Config.PumpModels[GetEntityModel(object)] then
-			table.insert(fuelPumps, object)
+			fuelPumps[#fuelPumps+1] = object
 		end
-
 		success, object = FindNextObject(handle, object)
 	until not success
 
