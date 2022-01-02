2cf49b288aec287311ee816b8e6d8cb7f29726df
vanillaa <80897991+evanillaa@users.noreply.github.com>
2022-01-02 10:01:59 +0200

    add tax functions globaly

--- source/fuel_server.lua
+++ source/fuel_server.lua
@@ -1,12 +1,17 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+local function GlobalTax(value)
+	local tax = (value / 100 * Config.GlobalTax)
+	return tax
+end
+
 -- refuel vehicle menu
 RegisterNetEvent("lj-fuel:server:OpenMenu", function (amount, inGasStation)
 	local src = source
 	if not src then return end
 	local player = QBCore.Functions.GetPlayer(src)
 	if not player then return end
-	local tax = QBCore.Functions.GlobalTax(amount)
+	local tax = GlobalTax(amount)
 	local total = math.ceil(amount + tax)
 	if inGasStation == true then
 	TriggerClientEvent('qb-menu:client:openMenu', src, {
