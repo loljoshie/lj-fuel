84801ac04b68d50519b1bcea2cef8fbc1d29bea4
loljoshie <91661118+loljoshie@users.noreply.github.com>
2022-01-29 23:23:54 -0700

    1.3

---
+++ server/server.lua
@@ -0,0 +1,53 @@
+-- Variables
+
+local QBCore = exports['qb-core']:GetCoreObject()
+
+-- Functions
+
+local function GlobalTax(value)
+	local tax = (value / 100 * Config.GlobalTax)
+	return tax
+end
+
+-- Server Events
+
+RegisterNetEvent("lj-fuel:server:OpenMenu", function (amount, inGasStation)
+	local src = source
+	if not src then return end
+	local player = QBCore.Functions.GetPlayer(src)
+	if not player then return end
+	local tax = GlobalTax(amount)
+	local total = math.ceil(amount + tax)
+	if inGasStation == true then
+		TriggerClientEvent('qb-menu:client:openMenu', src, {
+			{
+				header = 'Gas Station',
+				txt = 'The total cost is going to be: $'..total..' including taxes.' ,
+				params = {
+					event = "lj-fuel:client:RefuelVehicle",
+					args = total,
+				}
+			},
+		})
+	end
+		if inGasStation == false then
+			TriggerClientEvent('qb-menu:client:openMenu', src, {
+				{
+					header = 'Gas Station',
+					txt = 'The total cost is going to be: $'..total..' including taxes.' ,
+					params = {
+					event = "lj-fuel:client:RefuelVehicle",
+					args = total,
+				}
+			},
+		})
+	end
+end)
+
+RegisterNetEvent("lj-fuel:server:PayForFuel", function (amount)
+	local src = source
+	if not src then return end
+	local player = QBCore.Functions.GetPlayer(src)
+	if not player then return end
+	player.Functions.RemoveMoney('cash', amount)
+end)
