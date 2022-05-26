46b0fe91904b1b2852eed69d00109ca0b355f78c
loljoshie <91661118+loljoshie@users.noreply.github.com>
2022-05-26 14:00:57 -0600

    Delete locales directory

--- locales/de.lua
+++
@@ -1,9 +0,0 @@
-local Translations = {
-    notify = {
-        ["no_money"] = "Ich habe nicht genug Geld",
-        ["refuel_cancel"] = "Tanken abgebrochen",
-        ["jerrycan_full"] = "Dieser Kanister ist bereits voll",
-        ["vehicle_full"] = "Dieses Fahrzeug ist bereits voll",
-    }
-}
-Lang = Locale:new({phrases = Translations, warnOnMissing = true})
